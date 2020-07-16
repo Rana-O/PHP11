d7ef8c15b9b610038c902919b8049669d70fb553
Rana-O <r.000918000@gmail.com>
2020-07-16 12:58:15 +0900

    whole-filess

---
+++ sample/resources/views/admin/news/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="viewport" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNewsEdit</title>
+    </head>
+    <body>
+        <h1>Myニュース編集画面</h1>
+    </body>
+</html>        
